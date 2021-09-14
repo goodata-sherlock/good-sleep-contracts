4d57ec94a8094044f541e4b6c7a2f6994ec9c00c
goodata-ivan <ivanz@goodata.org>
2021-09-14 17:21:56 +0800

    Indexed events of Reward contract

--- contracts/IReward.sol
+++ contracts/IReward.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.0;
 
 // interface IReward is IERC721, IERC721Metadata {
 interface IReward {
-    event Feeding(uint256 tokenId, uint256 amount);
-    event MultiplierUpdated(uint256 oldMultiplier, uint256 newMultiplier);
+    event Feeding(uint256 indexed tokenId, uint256 amount);
+    event MultiplierUpdated(uint256 indexed oldMultiplier, uint256 indexed newMultiplier);
     event Withdrawal(uint256 indexed tokenId, address indexed to, uint256 amount);
 
     struct FeedParam {
