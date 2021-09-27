55fa191df30454203af3a9be7adbeed3c70e6680
goodata-ivan <ivanz@goodata.org>
2021-09-27 10:36:35 +0800

    Update config

--- src/helper.ts
+++ src/helper.ts
@@ -3,8 +3,8 @@ import {
     BigInt
 } from '@graphprotocol/graph-ts'
 
-const SleepAvatarNFT = '0x2338B7e8189bE74896cD892411A6AFfcDEfC7fa9'
-const AppearanceAvatarNFT = '0xbCCab9aDf117C22fee8c0805890bB23f2407316D'
+const SleepAvatarNFT = '0x6359Ee5b6000169a091b7eb243d5e219c0Ffc7BF'
+const AppearanceAvatarNFT = '0xe3a855b650Be77697c1d987f1FB547a7d7167A03'
 
 let NFTWhiteList: Array<string> = [
     SleepAvatarNFT,
